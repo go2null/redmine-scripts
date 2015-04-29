4e8cc59d1630715125edc9272e800814bffad410
go2null <1t1is2@gmail.com>
2015-04-29 16:36:03 +0000

    ensure utf8_unicode_ci used in MySQL; fix "rails" script location for Redmine 3.x

--- new_redmine.sh
+++ new_redmine.sh
@@ -12,7 +12,7 @@
 
 # ensure you are in the parent directory of the new redmine install
 mkdir -p $REDMINE_PARENT_DIR
-cd $REDMINE_PARENT_DIR
+pushd $REDMINE_PARENT_DIR
 
 # Step 1 - Redmine application
 
@@ -34,8 +34,12 @@ echo "Version '$TAG' checked out to branch '$BRANCH'"
 # create database and user (if user doesn't exist)
 echo "Enter your MySQL/MariaDB 'root' password:"
 mysql -u root -p -e "
+	CHARSET utf8;
+	SET NAMES utf8 COLLATE utf8_unicode_ci;
+	SET character_set_server = utf8;
+	SET collation_server     = utf8_unicode_ci;
   DROP DATABASE IF EXISTS redmine_$BRANCH;
-  CREATE DATABASE redmine_$BRANCH CHARACTER SET utf8;
+  CREATE DATABASE redmine_$BRANCH DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;
   GRANT ALL PRIVILEGES ON redmine_$BRANCH.* TO 'redmine'@'localhost' IDENTIFIED BY '';
 "
 echo "Database 'redmine_$BRANCH' created and user 'redmine' given full permission."
@@ -48,23 +52,39 @@ sed -i "s|\(database: redmine\)$|\1_$BRANCH|" config/database.yml
 echo "database.yml updated."
 
 # Step 4 - Dependencies installation¶
-echo "Enter your 'sudo' password if requested:"
-sudo gem install bundler
+if which sudo >/dev/null 2>&1; then
+	echo "Enter your 'sudo' password if requested:"
+ sudo	gem install bundler
+else
+	gem install bundler
+fi
 bundle install --no-deployment --without development test
 
 # Step 5 - Session store secret generation
 bundle exec rake generate_secret_token
 
 # Step 6 - Database schema objects creation
-RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production                 bundle exec rake db:migrate
 
 # Step 7 - Database default data set
 RAILS_ENV=production REDMINE_LANG=en bundle exec rake redmine:load_default_data
 
 # Step 8 - File system permissions
-mkdir -p tmp tmp/pdf public/plugin_assets
+mkdir -p tmp/pdf public/plugin_assets
 sudo chown -R $REDMINE_USER:$REDMINE_GROUP files log tmp public/plugin_assets
 sudo chmod -R 755 files log tmp public/plugin_assets
 
 # Step 9 - Test the installation
-bundle exec ruby script/rails server webrick -e production
+# open default browser before starying server
+#	to allow leaving server in foreground (to allow CTRL+C break)
+if which xdg-open >/dev/null 2>&1; then
+	xdg-open http://localhost:3000/ &
+fi
+# test for new location of 'rails' script (Redmine 3.0), falling back to old location.
+if [ -e bin/rails ]; then
+	bundle exec ruby bin/rails server webrick -e production
+else
+	bundle exec ruby script/rails server webrick -e production
+fi
+# cleanup
+popd
